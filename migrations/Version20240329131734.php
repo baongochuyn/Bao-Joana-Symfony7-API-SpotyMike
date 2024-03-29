e6832b97acbac50dbce73b4b7888e551cafab53c
BaoNgoc <baongochuynh.hbn@gmail.com>
2024-03-29 14:27:15 +0100

    modify user entity

---
+++ migrations/Version20240329131734.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240329131734 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD lastname VARCHAR(55) NOT NULL, ADD sexe ENUM("F", "M") NOT NULL, ADD date_birth DATETIME NOT NULL, CHANGE name firstname VARCHAR(55) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD name VARCHAR(55) NOT NULL, DROP firstname, DROP lastname, DROP sexe, DROP date_birth');
+    }
+}
